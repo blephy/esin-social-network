293def13eb4ec7be6ff2ac038bb851c4f447bffa
Allan Dollé <hello@allandolle.fr>
2023-01-20 09:24:34 +0100

    chore: connect header with all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Button from '@mui/material/Button';
+import HeaderMy from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Hello World</Button>
+      <HeaderMy />
     </div>
   );
 }
