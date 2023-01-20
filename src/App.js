0ff40efac06fcde4d39689ad3f5fe5e16bdef025
MartinFEMENIAS <martinfemenias5@gmail.com>
2023-01-20 11:32:30 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import HeaderMy from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Button variant="contained">Hello World</Button>
+      <HeaderMy />
     </div>
   );
 }
-
 export default App;
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,21 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Grid';
+import Logo from '../Logo/Logo'
+import SearchBar from '../SearchBar/SearchBar'
+import Avatar from '../Avatar/Avatar'
+
 function Header() {
   return (
       <Grid container columns={2}>
         <Grid Header={8}>
-          <Item><Header /></Item>
+          <Item><Logo /></Item>
         </Grid>
         <Grid flexGrow={1} Header={4}>
-          <Item>Header</Item>
+          <Item><SearchBar /></Item>
+        </Grid>
+        <Grid Header={8}>
+          <Item><Avatar /></Item>
         </Grid>
       </Grid>
   );
