aef569bc9afef31033d8435317bd73872435b37a
inessben <106515002+inessben@users.noreply.github.com>
2023-01-20 09:12:53 +0100

    add avatar component (#4)

---
+++ src/component/Avatar/Avatar.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import { deepPurple } from '@mui/material/colors';
+
+export default function LetterAvatars() {
+  return (
+      <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+  );
+}
