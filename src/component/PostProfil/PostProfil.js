7a6f946d7436b8d769fd7c287bf08425d21a3b13
Yohann-Hamon <hamon.yohann0416@gmail.com>
2023-01-20 12:20:13 +0100

    added img, username for the Post, and css(empty)

---
+++ src/component/PostProfil/PostProfil.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+import "./PostProfil.css";
+
+function PostProfil(props) {
+
+    const username = {item.lastName} + {item.firstName};
+
+  return (
+        <div className='profil-container'>
+            <img url="" className='profil-pic'></img>
+            <p>`username`</p>
+        </div>
+    );
+
+}
