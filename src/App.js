a8ea9f2aa7ca7e46823d362edbf6cb211dbf7371
Bel-kun <bellaire.vivien22@gmail.com>
2023-01-27 04:49:55 -0400

    Component maj

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import HeaderMy from './component/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <HeaderMy />
-      <TextareaValidator />
+
     </div>
   );
 }
---
+++ src/component/PostAction/Like.js
@@ -0,0 +1,21 @@
+import IconButton from '@mui/joy/IconButton';
+
+
+<IconButton
+>
+  <FavoriteBorder />
+</IconButton>
+
+
+function Like () 
+{
+    return
+    (
+        <IconButton
+        >
+        <FavoriteBorder />
+        </IconButton>
+    );
+}
+
+export default Like
