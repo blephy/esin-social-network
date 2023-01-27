af7ff09853e87f2f88066a2e421edcaf34a49fc0
Allan Dollé <hello@allandolle.fr>
2023-01-27 09:31:48 +0100

    chore: add api

--- src/component/Logo/Logo.js
+++ src/component/Logo/Logo.js
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 
 function Logo() {
   return (
-    <Typography className="typo" variant="h4" component="h2">
+    <Typography className="typo" variant="h5" component="h2">
       Yggdrasil
     </Typography>
   );
