10924f3711ea5f84adf035e30c2aedcd32819137
Allan Dollé <hello@allandolle.fr>
2023-02-03 13:25:09 +0100

    setup user view and edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from './component/Header/Header';
+import UserList from './component/UserList/UserList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <UserList />
     </div>
   );
 }
